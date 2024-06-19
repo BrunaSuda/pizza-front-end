b8091900f7be7d0d2a69460d35040f21dd298992
Jonatas <jonatasmateuscontato@gmail.com>
2024-06-19 18:31:07 -0300

    controle de navbar para usuário logado

--- pizza-delivery/src/app/app.component.ts
+++ pizza-delivery/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pizza-delivery';
+  enableNavbar: boolean = false;
+  authService = inject(AuthService);
+
+  ngOnInit() {
+    this.authService.userLoginEmitter.subscribe(
+      event => this.enableNavbar = event
+    );
+  }
 }
---
+++ pizza-delivery/src/app/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+}
