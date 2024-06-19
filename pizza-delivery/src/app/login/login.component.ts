5fc6fcedc0ef6b348cf42de3244c09648f32713d
Jonatas <jonatasmateuscontato@gmail.com>
2024-06-19 16:15:18 -0300

    página login com rota habilitada

--- pizza-delivery/src/app/login/login.component.ts
+++ pizza-delivery/src/app/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    login: new FormControl('', Validators.required),
+    senha: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
 
+  onSubmit() {
+    alert('Formulário submetido para o servidor');
+  }
 }
